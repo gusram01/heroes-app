3b9597a4e44d16b006d2cabcdb5c3cec3d6a98bb
Gus Ramírez <g.ram.bt@hotmail.com>
2020-12-22 16:53:02 -0600

    update dependencies

--- src/components/heroes/HeroesList.tsx
+++ src/components/heroes/HeroesList.tsx
@@ -2,32 +2,28 @@ import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 import HeroCard from './HeroCard';
 
-interface ChildComponentProps  {
-  /* Parent component's props*/ 
-  'id'?: string;
-  'superhero'?: string;
-  'publisher'?: string;
-  'alter_ego'?: string;
-  'first_appearance'?: string;
-  'characters'?: string;
+interface ChildComponentProps {
+  /* Parent component's props*/
+  id?: string;
+  superhero?: string;
+  publisher?: string;
+  alter_ego?: string;
+  first_appearance?: string;
+  characters?: string;
 }
 
-export default function HeroesList ( {publisher}:ChildComponentProps ) {
-
-  const heroes = useMemo(() => getHeroesByPublisher(publisher!), [publisher]);
+export default function HeroesList({ publisher }: ChildComponentProps) {
+  const heroes = useMemo(() => getHeroesByPublisher(publisher as string), [
+    publisher,
+  ]);
 
   // const heroes = getHeroesByPublisher(publisher!);
 
   return (
-    <div className="card-columns card__fade">
-      {
-        heroes.map(hero=>(
-          <HeroCard
-           key={hero.id}
-           {...hero}
-           />
-        ))
-      }
+    <div className='card-columns card__fade'>
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
     </div>
   );
-};
+}
